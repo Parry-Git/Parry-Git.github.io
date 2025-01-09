661d7632e952211edaa1d539b9dab224c980b43b
Parry-Git <160721277+Parry-Git@users.noreply.github.com>
2025-01-09 05:51:49 +0000

    Deploying to gh-pages from @ Parry-Git/Parry-Git.github.io@11a1c99d18e768f9890327dc5d4694ef8ffab249 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-calendar",
+          title: "Calendar",
+          description: "Calendar for courses, class meetings, and other events.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/calendar/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
